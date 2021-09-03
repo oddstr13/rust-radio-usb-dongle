22483564aa74c39f78a57ad025746cce10ce964c
Odd Stråbø <oddstr13@openshell.no>
2021-09-04 01:46:39 +0200

    Integrate usb-serial echo example

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,15 @@ use core::sync::atomic::{self, Ordering};
 use cortex_m_rt::entry;
 use hal::clocks::{self, Clocks};
 use hal::ieee802154::{self, Channel, Packet, TxPower};
+use hal::pac::ficr;
 
 use core::panic::PanicInfo;
 use cortex_m::asm;
+
+// USB Serial
+use hal::usbd::{UsbPeripheral, Usbd};
+use usb_device::device::{UsbDeviceBuilder, UsbVidPid};
+use usbd_serial::{SerialPort, USB_CLASS_CDC};
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
@@ -28,60 +34,103 @@ fn main() -> ! {
         Clocks<clocks::ExternalOscillator, clocks::ExternalOscillator, clocks::LfOscStarted>,
     > = None;
     
-    if let Some(periph) = hal::pac::Peripherals::take() {
-        let clocks = Clocks::new(periph.CLOCK);
-        let clocks = clocks.enable_ext_hfosc();
-        let clocks = clocks.set_lfclk_src_external(clocks::LfOscConfiguration::NoExternalNoBypass);
-        let clocks = clocks.start_lfclk();
-        let _clocks = clocks.enable_ext_hfosc();
-        //let board = hal::init().unwrap();
+    let periph = hal::pac::Peripherals::take().unwrap();
+    let clocks = Clocks::new(periph.CLOCK);
+    let clocks = clocks.enable_ext_hfosc();
+    let clocks = clocks.set_lfclk_src_external(clocks::LfOscConfiguration::NoExternalNoBypass);
+    let clocks = clocks.start_lfclk();
+    let _clocks = clocks.enable_ext_hfosc();
+    //let board = hal::init().unwrap();
+    let clocks = unsafe { CLOCKS.get_or_insert(_clocks) };
 
-        let clocks = unsafe { CLOCKS.get_or_insert(_clocks) };
 
-        let mut radio = {
-            let mut radio = hal::ieee802154::Radio::init(periph.RADIO, clocks);
+    let usb_bus = Usbd::new(UsbPeripheral::new(periph.USBD, &clocks));
+    let mut serial = SerialPort::new(&usb_bus);
 
-            // set TX power to its maximum value
-            radio.set_txpower(ieee802154::TxPower::Pos8dBm);
-            log::debug!("Radio initialized and configured with TX power set to the maximum value");
-            radio
-        };
 
-        // these are the default settings of the DK's radio
-        // NOTE if you ran `change-channel` then you may need to update the channel here
-        radio.set_channel(Channel::_20); // <- must match the Dongle's listening channel
-        radio.set_txpower(TxPower::Pos8dBm);
+    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
+        .manufacturer("https://OpenShell.no")
+        .product("radio-usb-dongle")
+        .serial_number("TEST") // TODO: Use device ID
+        .device_class(USB_CLASS_CDC)
+        .max_packet_size_0(64) // (makes control transfers 8x faster)
+        .build();
+    
 
-        let mut packet = Packet::new();
+    let mut radio = {
+        let mut radio = hal::ieee802154::Radio::init(periph.RADIO, clocks);
 
-        // these three are equivalent
-        // let msg: &[u8; 5] = &[72, 101, 108, 108, 111];
-        // let msg: &[u8; 5] = &[b'H', b'e', b'l', b'l', b'o'];
-        let mut _msg = [b'H', b'e', b'l', b'l', b'o', b' ', b' '];
-        let msg: &mut[u8] = &mut _msg[..];
+        // set TX power to its maximum value
+        radio.set_txpower(ieee802154::TxPower::Pos8dBm);
+        log::debug!("Radio initialized and configured with TX power set to the maximum value");
+        radio
+    };
 
-        log::info!(
-            "sending: {}",
-            str::from_utf8(msg).expect("msg is not valid UTF-8 data")
-        );
+    // these are the default settings of the DK's radio
+    // NOTE if you ran `change-channel` then you may need to update the channel here
+    radio.set_channel(Channel::_20); // <- must match the Dongle's listening channel
+    radio.set_txpower(TxPower::Pos8dBm);
 
-        for n in 0..=9 {
+    let mut packet = Packet::new();
 
-            msg[6] = n + 48;
+    // these three are equivalent
+    // let msg: &[u8; 5] = &[72, 101, 108, 108, 111];
+    // let msg: &[u8; 5] = &[b'H', b'e', b'l', b'l', b'o'];
+    let mut _msg = [b'H', b'e', b'l', b'l', b'o', b' ', b' '];
+    let msg: &mut[u8] = &mut _msg[..];
 
-            packet.copy_from_slice(msg);
-            radio.send(&mut packet);
-        }
+    log::info!(
+        "sending: {}",
+        str::from_utf8(msg).expect("msg is not valid UTF-8 data")
+    );
 
-        // Turn off TX for the love of the spectrum!
-        radio.energy_detection_scan(1);
+    for n in 0..=9 {
+
+        msg[6] = n + 48;
+
+        packet.copy_from_slice(msg);
+        radio.send(&mut packet);
     }
+
+    // Turn off TX for the love of the spectrum!
+    radio.energy_detection_scan(1);
 
     log::info!("`dk::exit() called; exiting ...`");
 
     // force any pending memory operation to complete before the BKPT instruction that follows
     atomic::compiler_fence(Ordering::SeqCst);
     loop {
-        asm::bkpt()
+
+        if !usb_dev.poll(&mut [&mut serial]) {
+            continue;
+        }
+
+        let mut buf = [0u8; 64];
+
+        match serial.read(&mut buf) {
+            Ok(count) if count > 0 => {
+                // Echo back in upper case
+                for c in buf[0..count].iter_mut() {
+                    if 0x61 <= *c && *c <= 0x7a {
+                        *c &= !0x20;
+                    }
+                    // Stop on receiving Q
+                    if c == &b'Q' {
+                        asm::bkpt()
+                    }
+                }
+
+                let mut write_offset = 0;
+                while write_offset < count {
+                    match serial.write(&buf[write_offset..count]) {
+                        Ok(len) if len > 0 => {
+                            write_offset += len;
+                        }
+                        _ => {}
+                    }
+                }
+            }
+            _ => {}
+        }
     }
 }
